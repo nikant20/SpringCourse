87c59a0b2c6b0b61fa4b010bac854597fa97ae9a
NikantChaudhary <NChaudhary@opteamix.com>
2020-06-18 08:28:48 +0530

    Completed Paging and Sorting

--- ProductData/src/main/java/tech/nikant/springdata/product/repos/ProductRepository.java
+++ ProductData/src/main/java/tech/nikant/springdata/product/repos/ProductRepository.java
@@ -2,13 +2,14 @@ package tech.nikant.springdata.product.repos;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import tech.nikant.springdata.product.entities.Product;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Integer> {
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
 		List<Product> findByName(String name);
 		
 		List<Product> findByNameAndDescription(String name, String description);
@@ -19,5 +20,5 @@ public interface ProductRepository extends CrudRepository<Product, Integer> {
 		
 		List<Product> findByPriceBetween(Double price1, Double price2);
 		
-		List<Product> findByIdIn(List<Integer> ids);
+		List<Product> findByIdIn(List<Integer> ids, Pageable pageable);
 }
--- ProductData/src/test/java/tech/nikant/springdata/product/ProductDataApplicationTests.java
+++ ProductData/src/test/java/tech/nikant/springdata/product/ProductDataApplicationTests.java
@@ -10,6 +10,11 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 
 import tech.nikant.springdata.product.entities.Product;
 import tech.nikant.springdata.product.repos.ProductRepository;
@@ -19,7 +24,7 @@ class ProductDataApplicationTests {
 
 	@Autowired
 	ProductRepository repository;
-	
+
 	@Test
 	public void testCreate() {
 		Product product = new Product();
@@ -27,82 +32,105 @@ class ProductDataApplicationTests {
 		product.setName("OnePlus");
 		product.setDescription("Awesome");
 		product.setPrice(1000d);
-		
+
 		repository.save(product);
 	}
-	
+
 	@Test
 	public void testRead() {
 		Optional<Product> findById = repository.findById(101);
-		if(findById.isPresent()) {
+		if (findById.isPresent()) {
 			Product product = findById.get();
 			assertNotNull(product);
 			assertEquals("OnePlus", product.getName());
 		}
 	}
-	
+
 	@Test
 	public void testUpdate() {
-		Optional<Product> optional  = repository.findById(1);
-		
-		if(optional.isPresent()) {
+		Optional<Product> optional = repository.findById(1);
+
+		if (optional.isPresent()) {
 			Product product = optional.get();
 			product.setName("OnePlus 7t");
 			product.setDescription("Awesome");
 			repository.save(product);
-		} 
+		}
 	}
-	
+
 	@Test
 	public void testDelete() {
-		if(repository.existsById(101)) {
+		if (repository.existsById(101)) {
 			repository.deleteById(101);
 		}
-		
+
 	}
-	
+
 	@Test
 	public void test_findByName() {
 		List<Product> product = repository.findByName("OnePlus");
 		assertNotNull(product);
 		product.forEach(p -> System.out.println(p.getName()));
 	}
-	
+
 	@Test
 	public void test_findByNameAndDesc() {
-		List<Product> product = repository.findByNameAndDescription("OnePlus","From OnuPlus");
+		List<Product> product = repository.findByNameAndDescription("OnePlus", "From OnuPlus");
 		assertNotNull(product);
 		product.forEach(p -> System.out.println(p.getName()));
 	}
-	
+
 	@Test
 	public void test_findByPriceGraterThan() {
 		List<Product> product = repository.findByPriceGreaterThanEqual(1d);
 		assertNotNull(product);
 		product.forEach(p -> System.out.println(p.getName()));
 	}
-	
+
 	@Test
 	public void test_findByPriceBetween() {
 		List<Product> product = repository.findByPriceBetween(1d, 100d);
 		assertNotNull(product);
 		product.forEach(p -> System.out.println(p.getName()));
 	}
-	
+
 	@Test
 	public void test_findByNameContains() {
 		List<Product> product = repository.findByDescriptionLike("From%");
 		assertNotNull(product);
 		product.forEach(p -> System.out.println(p.getName()));
 	}
-	
+
 	@Test
 	public void test_findByIdIn() {
 		List<Integer> ids = new ArrayList<Integer>();
+		ids.add(1);
 		ids.add(2);
 		ids.add(3);
-		List<Product> product = repository.findByIdIn(ids);
+		ids.add(4);
+		Pageable pageable = PageRequest.of(0, 2);
+		List<Product> product = repository.findByIdIn(ids, pageable);
 		assertNotNull(product);
 		product.forEach(p -> System.out.println(p.getName()));
+	}
+
+	@Test
+	public void test_findAllPaging() {
+		Pageable pageable = PageRequest.of(1, 2);
+		Page<Product> results = repository.findAll(pageable);
+		assertNotNull(results);
+		results.forEach(p -> System.out.println(p.getName()));
+	}
+	
+	@Test
+	public void test_findAllSorting() {
+		repository.findAll(Sort.by(Direction.ASC, "price", "name")).forEach(p -> System.out.println(p.getName()));
+		repository.findAll(Sort.by(new Sort.Order(Direction.ASC, "name"), new Sort.Order(Direction.DESC, "price")));
+	}
+	
+	@Test
+	public void test_findAllPagingAndSorting() {
+		Pageable pageable = PageRequest.of(0, 2, Direction.DESC, "name");
+		repository.findAll(pageable).forEach(p -> System.out.println(p.getName()));
 	}
 }
